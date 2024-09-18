1debf8a9b09feebd7941f96ceaab429ba0961e9f
Nitesh Goad <goadnitesh4813@gmail.com>
2024-09-18 14:17:33 +0530

    First-commit

---
+++ middlewares/auth.js
@@ -0,0 +1,29 @@
+const { getUser } = require("../service/auth");
+
+function checkForAuthentication(req, res, next) {
+  const tokencookie = req.cookies?.token;
+  req.user = null;
+
+  if (!tokencookie) return next();
+
+  const token = tokencookie;
+  const user = getUser(token);
+
+  req.user = user;
+  return next();
+}
+
+function restrictTo(roles = []) {
+  return function (req, res, next) {
+    if (!req.user) return res.redirect("/login");
+
+    if (!roles.includes(req.user.role)) return res.end("UnAuthorized");
+
+    return next();
+  };
+}
+
+module.exports = {
+  checkForAuthentication,
+  restrictTo,
+};
